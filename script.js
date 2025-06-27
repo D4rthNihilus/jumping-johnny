f0229104d2c3cc4124bc8e61a5de0a1fd6138846
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-06-27 12:11:03 -0300

    versão 2.6 (current skin safe)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const startButton = document.querySelector('.start');
 const customButton = document.querySelector('.customButton');
+let lifeSkin = localStorage.getItem('skin');
 let skins = document.querySelector('.skins');
 const johnny = document.querySelector('.johnny');
-let johnnyGameOver = 'skins/joneghost.png';
+johnny.src = lifeSkin;
+let johnnyGameOver = localStorage.getItem('ghost');
 const clarice = document.querySelector('.clarice');
 const sky = document.querySelector('div.game');
 const sun = document.querySelector('.sun');
@@ -17,11 +19,11 @@ function start() {
     grass.style.animation = 'walking 2.5s infinite linear';
     startButton.style.display = 'none';
     customButton.style.display = 'none';
-    clarice.style.animation = 'clarice-animation 2.5s infinite linear';
+    clarice.style.animation = 'clarice-animation 2s infinite linear';
     let contador = setInterval(function(){
-                score++;
-                divscore.innerHTML = `${score} Clarices`;
-            }, 2500);
+            score++;
+            divscore.innerHTML = `${score} Clarices`;
+        }, 2000);
     const jump = () => {
         johnny.classList.add('jump');
 
@@ -40,12 +42,12 @@ function start() {
             clarice.style.left = `${claricePosition}px`;
 
             johnny.style.animation = 'death 3s ease-in-out';
-            setTimeout (function() {
-                johnny.style.display = 'none';
-            }, 3000);
 
             johnny.src = johnnyGameOver;
             johnny.style.opacity = '0.8';
+            setTimeout(function () {
+                johnny.style.display = 'none';
+            }, 3000);
 
             clearInterval(loop);
             clearInterval(contador);
@@ -63,6 +65,9 @@ function start() {
 
     document.addEventListener('keydown', jump);
     document.addEventListener('touchstart', jump);
+
+    localStorage.setItem('skin', lifeSkin);
+    localStorage.setItem('ghost', johnnyGameOver);
 }
 
 function customize() {
@@ -71,15 +76,16 @@ function customize() {
 
 function joneNovo() {
     characterIndex = 1;
-    johnny.src='skins/jone-Novo.png';
+    lifeSkin ='skins/jone-Novo.png';
+    johnny.src = lifeSkin;
     johnnyGameOver = 'skins/joneghost.png';
     skins.style.display = 'none';
 }
 
 function joneClassico() {
     characterIndex = 2;
-    johnny.src='skins/jone-classico.png';
-    johnny.style.bottom = '30px';
+    lifeSkin ='skins/jone-classico.png';
+    johnny.src = lifeSkin;
     johnnyGameOver = 'skins/jnclasghost.png'
     skins.style.display = 'none';
 
@@ -87,26 +93,28 @@ function joneClassico() {
 
 function joneChaves() {
     characterIndex = 3;
-    johnny.src='skins/jone-chaves.png';
+    lifeSkin ='skins/jone-chaves.png';
+    johnny.src = lifeSkin;
     johnnyGameOver = 'skins/jnchavesghost.png';
     skins.style.display = 'none';
 }
 
 function jonoel() {
     characterIndex = 4;
-    johnny.src='skins/jone-noel.png';
+    lifeSkin = 'skins/jone-noel.png';
+    johnny.src = lifeSkin;
     johnnyGameOver = 'skins/jonoelghost.png';
     skins.style.display = 'none';
 }
 
 function clone() {
     characterIndex = 5;
-    johnny.src='skins/clone.png';
+    lifeSkin = 'skins/clone.png';
+    johnny.src = lifeSkin;
     johnnyGameOver = 'skins/cloneghost.png';
     skins.style.display = 'none';
 }
 
 function reload() {
-    reloadButton.style.display = 'none';
-    
+    location.reload();
 }
